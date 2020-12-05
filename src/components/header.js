9dde9e060b537abf6b04ee8422db189c4bc32374
fe-amazu <tenzyu.on@gmail.com>
2020-12-05 21:20:38 +0900

    use Link from gatsby

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,8 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default () => (
   <header>
-    <h1>tenzyu.com</h1>
+      <Link to={"/"}><h1>tenzyu.com</h1></Link>
   </header>
 )
