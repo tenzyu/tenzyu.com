837dedfc3866146a55e33de9be58c0c0a96f0137
fe-amazu <tenzyu.on@gmail.com>
2020-12-05 22:13:12 +0900

    wrote header

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
 
+const moreStyles = {
+  fontFamily: "arial black, Helvetica Neue, sans-serif",
+}
+
 export default () => (
-  <header>
-      <Link to={"/"}><h1>tenzyu.com</h1></Link>
+  <header style={moreStyles} class="flex sticky z-50 top-0 left-0 items-center h-16 border-b opacity-90 bg-white">
+      <Link to={"/"}><div class="mt-1 mb-2 text-2xl ml-6">tenzyu.com</div></Link>
   </header>
 )
