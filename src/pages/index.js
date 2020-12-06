7cc4f94d79d37c09ebd613b5decfd92996ea1e15
fe-amazu <tenzyu.on@gmail.com>
2020-12-06 15:45:09 +0900

    renamed, added(wishlist and wantedly) and minor fix(styles)

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,46 +45,58 @@ const facts = [
 
 const links = [
   {
-    image: "account-github.png",
+    image: "link-github.png",
     name: "GitHub",
     meta: "tenzyu",
     url: "https://github.com/tenzyu"
   },
   {
-    image: "account-youtube.png",
+    image: "link-youtube.png",
     name: "YouTube",
     meta: "tenzyu",
     url: "https://www.youtube.com/channel/UCnlpsS3qhWUhNwd2yc_D4cw"
   },
   {
-    image: "account-spotify.png",
+    image: "link-spotify.png",
     name: "Spotify",
     meta: "tenzyu",
     url: "https://open.spotify.com/user/kuv8bpy5ocbl8mwspxymi1six?si=6BAjQnRHQdan6qrCOkudTg"
   },
   {
-    image: "account-twitter.png",
+    image: "link-twitter.png",
     name: "Twitter",
     meta: "@tenzyumasuda",
     url: "https://twitter.com/tenzyumasuda"
   },
   {
-    image: "account-instagram.png",
+    image: "link-instagram.png",
     name: "Instagram",
     meta: "@tenzyumasuda",
     url: "https://instagram.com/tenzyumasuda"
   },
   {
-    image: "account-market.png",
+    image: "link-link.png",
+    name: "Wantedly",
+    meta: "升田 天珠",
+    url: "https://www.wantedly.com/users/144791642"
+  },
+  {
+    image: "link-market.png",
     name: "メルカリ",
     meta: "升田 天珠",
     url: "https://www.mercari.com/jp/u/694596800/"
   },
   {
-    image: "account-discord.png",
+    image: "link-discord.png",
     name: "Discord",
     meta: "Click to join our server.",
     url: "https://discord.gg/8DrwSxd"
+  },
+  {
+    image: "link-wishlist.png",
+    name: "Amazon",
+    meta: "Click to jump to my wishlist",
+    url: "https://www.amazon.jp/hz/wishlist/ls/34YAIK53SZJQD?ref_=wl_share"
   },
 ]
 
