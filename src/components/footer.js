d3e26971b9da93718dfbea6d0442ad4abd2455ac
tenzyu <tenzyu.on@gmail.com>
2021-01-02 00:22:01 +0900

    remove 2020

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,6 @@ import React from "react"
 
 export default () => (
   <footer class="mt-4 flex justify-end border-t mb-5">
-    <p class="font-thin text-xs mr-5 pt-1">© 2020 Tenzyu Masuda.</p>
+    <p class="font-thin text-xs mr-5 pt-1">© Tenzyu Masuda.</p>
   </footer>
 )
