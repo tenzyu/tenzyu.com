89425b5cb4d28e64e32bcedd1ceea97cb2843197
tenzyu <tenzyu.on@gmail.com>
2021-01-13 22:24:02 +0900

    improved the copyright

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,12 @@
 import React from "react"
 
+const createdYear = "2020"
+const currentYear = new Date().getFullYear()
+const authorName = "Tenzyu Masuda"
+const copyright = `© ${createdYear} - ${currentYear} ${authorName}`
+
 export default () => (
   <footer class="mt-4 py-8 flex justify-center border-t">
-    <p class="font-thin text-xs">© Tenzyu Masuda</p>
+    <p class="font-thin text-xs">{copyright}</p>
   </footer>
 )
