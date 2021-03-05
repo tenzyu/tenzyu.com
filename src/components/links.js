783a901253af0e1b54b1ebc039644f452637de79
tenzyu <tenzyu.on@gmail.com>
2021-03-05 14:48:46 +0900

    added space

--- src/components/links.js
+++ src/components/links.js
@@ -47,7 +47,7 @@ const links = [
 
 const Links = () => (
   <Section title="Links">
-    <ul class="sm:grid sm:grid-cols-2 sm:gap-x-4">
+    <ul class="grid gap-y-2 sm:grid-cols-2 sm:gap-x-4">
       {links.map((link) => (
         <li>
           <a
