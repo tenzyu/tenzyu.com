8aa26b0fdf1a5626b673f2741f3ba1ee7f425853
tenzyu <tenzyu.on@gmail.com>
2021-03-10 23:39:35 +0900

    added title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import React from 'react';
 import { MyContents } from '../components/MyContents';
 import { MyHistories } from '../components/MyHistories';
@@ -6,6 +7,9 @@ import { MyProfile } from '../components/MyProfile';
 
 const IndexPage: React.FC = () => (
   <>
+    <Head>
+      <title>Home - tenzyu.com</title>
+    </Head>
     <MyProfile />
     <MyContents />
     <MyLinks />
